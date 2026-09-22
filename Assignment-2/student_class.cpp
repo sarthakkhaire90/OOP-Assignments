ec5d0baa827dfa97568a37bd43efe579a4065f6e
sarthakkhaire90 <sarthakkhaire90@gmail.com>
2026-09-22 17:41:38 +0530

    Update student_class.cpp

--- Assignment-2/student_class.cpp
+++ Assignment-2/student_class.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Student {
+private:
+    string studentname;
+    int rollno;
+    int div;
+    float marks;
+
+public:
+    void input() {
+        studentname = "sarthak";
+        rollno = 25;
+        div = 2;
+        marks = 96;
+    }
+
+    void display() {
+        cout << "Student Name : " << studentname << endl;
+        cout << "Roll No   : " << rollno << endl;
+        cout << "div : " << div << endl;
+        cout << "marks   : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+    s.input();
+    s.display();
+
+    return 0;
+}
